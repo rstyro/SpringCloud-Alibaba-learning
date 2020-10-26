f3091b786de657bb691b9a0244695d081dc367ed
rstyro <1006059906@qq.com>
2020-10-26 11:45:58 +0800

    init

---
+++ springcloud-nacos-discovery-consumer/src/main/java/top/lrshuai/nacos/ConsumerApplication.java
@@ -0,0 +1,12 @@
+package top.lrshuai.nacos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
