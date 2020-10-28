84927fba716ac90e807c0f127b0cb264018d5e52
rstyro <1006059906@qq.com>
2020-10-28 14:50:08 +0800

    springcloud-alibaba之nacos配置中心

---
+++ springcloud-nacos-config/src/main/java/top/lrshuai/nacos/NacosConfigApplication.java
@@ -0,0 +1,12 @@
+package top.lrshuai.nacos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigApplication.class, args);
+    }
+}
---
+++ springcloud-nacos-config/src/main/java/top/lrshuai/nacos/config/UserConfig.java
@@ -0,0 +1,27 @@
+package top.lrshuai.nacos.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author rstyro
+ */
+//@RefreshScope
+//@Component
+public class UserConfig {
+    @Value("${user.name}")
+    private String name;
+
+    @Value("${user.age}")
+    private int age;
+
+    @Override
+    public String toString() {
+        return "UserConfig{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
